170bd9a6fc704d1d9d3aded1336aa12673c66237
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-07 21:13:01 +0600

    Single and Multiple exports

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import add from '../../utilities/calculate';
 
 const Cosmetics = () => {
+    const first = 55;
+  const second = 45;
+  const total = add(first, second);
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
+            <p>{total}</p>
         </div>
     );
 };
--- src/utilities/calculate.js
+++ src/utilities/calculate.js
@@ -1,3 +1,12 @@
 const add = (first, second) =>{
     return first + second;
 }
+
+const multiply = (first, second) =>{
+    return first * second;
+}
+
+// export default add; - Single default export
+
+//Multiple exports declaration
+export {add, multiply};
