02a49eafdde1cd845c40fea1b2aadc836c055d41
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-07 21:15:56 +0600

    successfully import multiple module as utilities

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import add from '../../utilities/calculate';
+import { add, multiply } from '../../utilities/calculate';
+// import add from '../../utilities/calculate';
+
 
 const Cosmetics = () => {
     const first = 55;
   const second = 45;
   const total = add(first, second);
+  const gun = multiply(first, second);
     return (
         <div>
             <h1>Welcome to my cosmetics store</h1>
             <p>{total}</p>
+            <p>{gun}</p>
         </div>
     );
 };
