6387f450c68640b49b2cae93cf84119bd697f868
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-08 17:44:48 +0600

    monirul fake data will need to work later

--- src/components/Monirul/Recap/Recap.js
+++ src/components/Monirul/Recap/Recap.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 const Recap = (props) => {
-    const {name,price} = props.mobile;
+    const {id, name,price} = props.mobile;
+    
+    const passAddToCart = () =>{
+        addToCart(id)
+    }
+
+    const addToCart = (id) =>{
+        console.log("clciked", id);
+    }
 
     return (
         <div>
             <p>{name}</p>
             <p>${price}</p>
-            <button>Add to Cart</button>
+            <button onClick={passAddToCart}>Add to Cart</button>
         </div>
     );
 };
