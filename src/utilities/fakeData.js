2b9b5862d75d7b2b07e6cc7db06ce0e2e3f61cc8
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-08 15:22:07 +0600

    JSON stringify and JSON parse

--- src/utilities/fakeData.js
+++ src/utilities/fakeData.js
@@ -1,14 +1,24 @@
 const dataDb = id =>{
-    const quantity = localStorage.getItem(id);
-    if(quantity){
-       console.log('already exists');
-       const newQuantity = parseInt(quantity) + 1;
-       localStorage.setItem(id, newQuantity);
+    let shoppingCart = {};
+
+    //Get the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart')
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
     }
     else{
-        localStorage.setItem(id, 1);
+        shoppingCart = {};
     }
-    
+    // Add quantity
+    const quantity = shoppingCart[id];
+    if(quantity){
+       const newQuantity = parseInt(quantity) + 1;
+       shoppingCart[id] = newQuantity;
+    }
+    else{
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 }
 
 export {dataDb};
