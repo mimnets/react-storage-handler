de950c2b7022991bb3a5a2ea6ef3854d18231e52
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-09 12:20:07 +0600

    simple intro to array reduce

--- src/utilities/calculate.js
+++ src/utilities/calculate.js
@@ -6,6 +6,10 @@ const multiply = (first, second) =>{
     return first * second;
 }
 
+const numbers = [34,56, 67, 23, 65, 78];
+const sumReducer = (previous, current) => previous + current;
+const total = numbers.reduce(sumReducer,0);
+
 // export default add; - Single default export
 
 //Multiple exports declaration
