3ba8d677c1f036f6efa1363a85d1e18729f5791f
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-09 12:43:09 +0600

    Count total with reducer(current,0)

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { add, multiply } from '../../utilities/calculate';
+import { add, getTotal, multiply } from '../../utilities/calculate';
 import Cosmetic from '../Cosmetic/Cosmetic';
 // import add from '../../utilities/calculate';
 
@@ -12,9 +12,14 @@ const Cosmetics = () => {
         .then(data => setCosmetics(data))
     },[])
 
+    const total = getTotal(cosmetics);
+
+    console.log(total)
+
     return (
         <div>
             <h1>Welcome to my cosmetics store - learn mock api</h1>
+            <p>Money need : {total}</p>
             {
                 cosmetics.map(cosmetic => <Cosmetic 
                     key={cosmetic.id} 
--- src/utilities/calculate.js
+++ src/utilities/calculate.js
@@ -6,11 +6,28 @@ const multiply = (first, second) =>{
     return first * second;
 }
 
-const numbers = [34,56, 67, 23, 65, 78];
-const sumReducer = (previous, current) => previous + current;
-const total = numbers.reduce(sumReducer,0);
+// const numbers = [34,56, 67, 23, 65, 78];
+// const sumReducer = (previous, current) => previous + current;
+// const total = numbers.reduce(sumReducer,0);
+// const items = [
+//     {id: 1, name: "Alta", price: 100},
+//     {id: 2, name: "kOLTA", price: 200},
+//     {id: 3, name: "bOLTA", price: 300},
+//     {id: 4, name: "Solta", price: 400}
+// ]
+
+// const itemSumReducer = (previous, current) => previous + current.price;
+// const itemTotal = items.reduce(itemSumReducer, 0);
+
+const getTotalPrice = products => {
+    const reducer = (previous, current) => previous + current.price;
+    const total = products.reduce(reducer, 0);
+    return total;
+}
 
 // export default add; - Single default export
 
 //Multiple exports declaration
-export {add, multiply};
+// export {add, multiply, getTotalPrice}; OR
+
+export {add, multiply, getTotalPrice as getTotal};
